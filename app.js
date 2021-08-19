7f8c937787fa64e021179ebd2e613f90c21b0161
Rian Khanafi <51011550+RianKhanafi@users.noreply.github.com>
2021-08-19 12:21:56 +0700

    defailt cors

--- app.js
+++ app.js
@@ -33,18 +33,18 @@ const app = express();
 //     "optionsSuccessStatus": 204
 //   }
 
-var allowlist = ["http:localhost:3000"];
-var corsOptionsDelegate = function (req, callback) {
-  var corsOptions;
-  if (allowlist.indexOf(req.header("Origin")) !== -1) {
-    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false }; // disable CORS for this request
-  }
-  callback(null, corsOptions); // callback expects two parameters: error and options
-};
+// var allowlist = ["http:localhost:3000"];
+// var corsOptionsDelegate = function (req, callback) {
+//   var corsOptions;
+//   if (allowlist.indexOf(req.header("Origin")) !== -1) {
+//     corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+//   } else {
+//     corsOptions = { origin: false }; // disable CORS for this request
+//   }
+//   callback(null, corsOptions); // callback expects two parameters: error and options
+// };
 
-app.use(cors(corsOptionsDelegate));
+// app.use();
 
 app.use(logger("dev"));
 app.use(express.json({ limit: "50mb" }));
@@ -54,7 +54,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use("/courses", coursesRouter);
+app.use("/courses", cors(), coursesRouter);
 app.use("/media", verifyToken, can("admin", "student"), mediaRouter);
 app.use("/orderss", ordersRouter);
 app.use("/orders", verifyToken, can("admin", "student"), orderPaymentRouter);
